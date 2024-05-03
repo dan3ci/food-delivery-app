5b2ad8e2a74a052ced28d284ac6f69a45f5b239a
dynekki <tremolodaneci@icloud.com>
2024-05-03 11:22:19 +0545

    food-menu items completed /1:01:01

---
+++ food-del/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt labore provident dolorum tempora? Obcaecati necessitatibus, maiores ab pariatur asperiores nam nisi accusamus iusto facilis! Deleniti commodi eius quod nam ex?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
